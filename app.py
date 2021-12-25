4a5f6d8b8dc8bce735251eeae6a5a9de0608e4a6
ChandanaReddy07 <b219021@iiit-bh.ac.in>
2021-12-25 15:47:17 +0530

    updated

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
-from flask import Flask
+from flask import Flask , Response ,render_template
+from base64 import b64encode
+
+import requests
+import json
+import os
+import random
+
+SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+SPOTIFY_SECRET_ID = os.getenv("SPOTIFY_SECRET_ID") 
+SPOTIFY_URL_NOW_PLAYING = "https://api.spotify.com/v1/me/player/currently-playing"
+SPOTIFY_URL_RECENTLY_PLAY = "https://api.spotify.com/v1/me/player/recently-played?limit=10"
+
+
+def getAuth():
+    return b64encode(f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_SECRET_ID}".encode()).decode("ascii")
+
 
 
 app=Flask(__name__)
